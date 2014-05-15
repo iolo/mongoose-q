030c08508bf1baf00cf5ffdb159300bdd7121773
Chris Trevino <chris.trevino@atsid.com>
2014-05-15 10:12:07 -0700

    minor cleanup

--- tests/mongooseq_default_test.js
+++ tests/mongooseq_default_test.js
@@ -4,7 +4,6 @@ return 'qDefault' + name.charAt(0).toUpperCase() + name.substring(1);
 };
 
 var commonTests = require('./common_tests', customMapper);
-var QProvider = require('../lib/promise_providers/q');
 var mongoose = require('mongoose');
 var mongooseQ = require('../lib')(mongoose, {
   spread: true,
