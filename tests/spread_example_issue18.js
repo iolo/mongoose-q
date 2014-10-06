24cbe97077234ee65690f7c52e94359100020b6c
Dongsu Jang <iolothebard@gmail.com>
2014-10-06 10:46:27 +0900

    example code for issue#18

---
+++ tests/spread_example_issue18.js
@@ -0,0 +1,13 @@
+// try this too
+// var mongoose = require('./index')();
+var mongoose = require('./index')(require('mongoose'),{spread:true});
+
+mongoose.connect('mongodb://localhost/test');
+
+var Tank = mongoose.model('Tank', new mongoose.Schema({ name: 'string', size: 'string'}));
+
+var small = new Tank({size: 'small'});
+small.saveQ().then(function(x,y){console.log('small with then', 'x=',x,'y=',y);}).done();
+
+var large = new Tank({size: 'large'});
+large.saveQ().spread(function(x,y){console.log('large with spread', 'x=',x,'y=',y);}).done();
